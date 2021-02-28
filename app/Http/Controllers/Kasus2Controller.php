afe857f6b12e894b3d57db3011a213be23798b50
AjrinNR <ajrinnr11@gmail.com>
2021-02-28 17:37:15 +0700

    hstng

--- app/Http/Controllers/Kasus2Controller.php
+++ app/Http/Controllers/Kasus2Controller.php
@@ -33,8 +33,8 @@ class Kasus2Controller extends Controller
     public function create()
     {
         
-
-        return view('kasus2.create');
+        $rw = Rw::all();
+        return view('kasus2.create',compact('rw'));
     }
 
     /**
@@ -74,7 +74,7 @@ class Kasus2Controller extends Controller
         $kasus2->jumlah_positif = $request->jumlah_positif;
         $kasus2->jumlah_sembuh = $request->jumlah_sembuh;
         $kasus2->jumlah_meninggal = $request->jumlah_meninggal;
-        $kasus2->tanggal = date('Y-m-d');
+        $kasus2->tanggal = $request->tanggal;
         $kasus2->save();
         return redirect()->route('kasus2.index')->with(['message' => 'Data Kasus Indonesia Berhasil Dibuat']);
     }
@@ -114,7 +114,6 @@ class Kasus2Controller extends Controller
     public function update(Request $request, $id)
     {
         $kasus2 = Kasus2::findOrFail($id);
-        $kasus2->id_rw = $request->id_rw;
         $kasus2->jumlah_positif = $request->jumlah_positif;
         $kasus2->jumlah_sembuh = $request->jumlah_sembuh;
         $kasus2->jumlah_meninggal = $request->jumlah_meninggal;
